abc9fd54e48487352faf5de91052fa5b1f0c8697
Michael Yang <themichaelyang@gmail.com>
2026-01-19 00:04:03 -0800

    add mandelbrot gpu scalar

--- lab1/mandelbrot_gpu.cu
+++ lab1/mandelbrot_gpu.cu
@@ -13,7 +13,31 @@ __global__ void mandelbrot_gpu_scalar(
     uint32_t max_iters,
     uint32_t *out /* pointer to GPU memory */
 ) {
-    /* your (GPU) code here... */
+    for (uint64_t i = 0; i < img_size; ++i) {
+        for (uint64_t j = 0; j < img_size; ++j) {
+            // Get the plane coordinate X for the image pixel.
+            float cx = (float(j) / float(img_size)) * 2.5f - 2.0f;
+            float cy = (float(i) / float(img_size)) * 2.5f - 1.25f;
+
+            // Innermost loop: start the recursion from z = 0.
+            float x2 = 0.0f;
+            float y2 = 0.0f;
+            float w = 0.0f;
+            uint32_t iters = 0;
+            while (x2 + y2 <= 4.0f && iters < max_iters) {
+                float x = x2 - y2 + cx;
+                float y = w - x2 - y2 + cy;
+                x2 = x * x;
+                y2 = y * y;
+                float z = x + y;
+                w = z * z;
+                ++iters;
+            }
+
+            // Write result.
+            out[i * img_size + j] = iters;
+        }
+    }
 }
 
 void launch_mandelbrot_gpu_scalar(
@@ -21,7 +45,7 @@ void launch_mandelbrot_gpu_scalar(
     uint32_t max_iters,
     uint32_t *out /* pointer to GPU memory */
 ) {
-    /* your (CPU) code here... */
+    mandelbrot_gpu_scalar<<<1, 1>>>(img_size, max_iters, out);
 }
 
 __global__ void mandelbrot_gpu_vector(
